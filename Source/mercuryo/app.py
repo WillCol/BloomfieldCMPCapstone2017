63788ba2ee594ae58a233bb08094de495c4adde1
marcelo3966 <marcelo_torrealba@bloomfield.edu>
2017-03-05 16:09:11 +0000

    generic table test

--- Source/mercuryo/app.py
+++ Source/mercuryo/app.py
@@ -94,6 +94,74 @@ def logout():
 	flash('You were just logged out!')
 	return redirect(url_for('login'))
 
+# generic page
+@app.route('/generic', methods=['GET', 'POST'])
+def generic():
+        #if request.method == 'GET':
+        taskID = request.args["id"]
+        if request.method == 'POST':
+                taskID = request.form["taskID"]
+                dateStart = request.form["dateStart"]
+                dateComplete = request.form["dateComplete"]
+                taskStatus = request.form["taskStatus"]
+                taskType = request.form["taskType"]
+
+                return "Request sucessfully submited!!"
+        else:
+
+                db = MySQLdb.connect(host="localhost",user="root",passwd="root",db="Inventory")
+                cur = db.cursor()
+                cur.execute("Select * from Calendar where TaskID = "+taskID)
+
+                task  = {"start" : "beginning"}
+                task.setdefault("def", [])
+                task["def"].append("Task ID")
+                task["def"].append("Date Started")
+                task["def"].append("Date Completed")
+                task["def"].append("Task Status")
+                task["def"].append("Task Type")
+
+                rowNum = 0
+                for row in cur.fetchall():
+                        task.setdefault(rowNum, [])
+                        task[rowNum].append(row[0])
+                        task[rowNum].append(row[1])
+                        task[rowNum].append(row[2])
+                        task[rowNum].append(row[3])
+                        task[rowNum].append(row[4])
+                        rowNum = rowNum + 1
+                return render_template('generic.html', task=task)
+
+# main table
+@app.route('/mainTable')
+def mainTable():
+                ## do calcs here, give send dictionary of objects
+                emp = {"start" : "beginning"}
+                emp.setdefault("def", [])
+                emp["def"].append("EmployeeID")
+                emp["def"].append("EmployeeName")
+                emp["def"].append("JobTitle")
+                emp["def"].append("EmployeeAddress")
+                emp["def"].append("EmployeeDepartment")
+
+                db = MySQLdb.connect(host="localhost",user="root",passwd="root",db="Inventory")
+                cur = db.cursor()
+                cur.execute("Select * from Employee")
+
+                rowNum = 0
+
+                for row in cur.fetchall():
+                        emp.setdefault(rowNum, [])
+                        emp[rowNum].append(row[0])
+                        emp[rowNum].append(row[1])
+                        emp[rowNum].append(row[2])
+                        emp[rowNum].append(row[3])
+                        emp[rowNum].append(row[4])
+                        rowNum = rowNum + 1
+                return render_template('mainTable.html', emp=emp)
+
+
+
 # start the server with the 'run()' method
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = False)
